c20acff41d242a716d6cf5c8a9648b89768005c0
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-06-20 14:44:23 +0200

    Elf on the shelf

--- data/classes/Veicoli.py
+++
@@ -1,8 +0,0 @@
-class Veicoli:
-    import datetime
-    def __init__(self, totali:int, in_uso:int,
-                 ritardo_o_ant:float, chiuso:bool) -> None:
-        self.totali = totali
-        self.in_uso = in_uso
-        self.ritardo_o_ant = ritardo_o_ant
-        self.chiuso = chiuso
