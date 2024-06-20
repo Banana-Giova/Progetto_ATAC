6dca77cb7b1db89fd911d1333abd473f0d5d007c
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-06-20 13:41:44 +0200

    Project advanced

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.home, name='home'),
+    path('bus/<int:line_number>', views.bus, name='bus'),
+    path('passengers', views.passengers, name='passengers'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,12 +2,37 @@ from django.shortcuts import render
 import django.http
 from django.views import View
 
+from .models import Passenger
+
 # Create your views here.
 
-class Home(View):
+"""class Home(View):
     def get(self, request):
         return render(request, "home.html")
 
     def post(self,request):
-        return render(request, "home.html")
+        return render(request, "home.html")"""
 
+bus_lines = [
+    {'id': 1, 'line_number': 451, 'name': 'Ponte Mammolo', 'stops': 18},
+    {'id': 2, 'line_number': 558, 'name': 'Gardenie', 'stops': 29},
+    {'id': 3, 'line_number': 777, 'name': 'Agricoltura', 'stops': 42},
+]
+
+def home(request):
+    context = {'bus_lines': bus_lines}
+    return render(request, "base/home.html", context)
+
+def bus(request, line_number):
+    req_bus = None
+    for bus in bus_lines:
+        if bus['line_number'] == int(line_number):
+            req_bus = bus
+            break
+    context = {'bus':bus}
+    return render(request, "base/bus.html", context)
+
+def passengers(request):
+    passengers = Passenger.objects.all()
+    context = {'passengers':passengers}
+    return render(request, "base/passengers.html", context)
