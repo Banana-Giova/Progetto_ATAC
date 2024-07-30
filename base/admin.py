acb7606a4ff06dc9f643302a1635333dab6588a7
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-07-30 13:14:21 +0200

    Refactoring

--- base/admin.py
+++ base/admin.py
@@ -4,6 +4,9 @@ from .models import *
 admin.site.register(Passenger)
 admin.site.register(Line)
 admin.site.register(Stop)
-admin.site.register(OrdinATAC)
 admin.site.register(Bus)
 admin.site.register(Driver)
+
+admin.site.register(OrdinATAC)
+admin.site.register(LiveTrATAC)
+admin.site.register(MultiDriver)
